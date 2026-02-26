2d705565d79d47dc1dca13aaea3f4b9831465f65
Kingsley <acciuskingsley@protonmail.com>
2026-02-25 19:50:13 -0500

    2026-02-25

---
+++ Module_4/P-1.cs
@@ -0,0 +1,88 @@
+using Sys = System;
+
+namespace P1
+{
+    public class Division
+    {
+        public void Excute()
+        {
+            Sys.Console.WriteLine("Enter The First Number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Sys.Console.WriteLine("Enter The Second Number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1/number2;
+        }
+        public void Display()
+        {
+            Sys.Console.WriteLine($"The Value is: {result}");
+        }
+    }
+    
+
+
+
+    class Program
+    {
+
+        public static void Logs(Sys.Exception e, bool words)
+        {
+                Sys.Console.WriteLine($"Error: {e.Message}");
+        }
+        
+        static void Main(string [] args)
+        {
+            Division div = new Division();
+
+            try
+            {
+                div.Excute();
+                div.Display();
+            }
+            catch (Sys.DivideByZeroException e)
+            {
+                Sys.Console.WriteLine("You Cannot Divide by Zero");
+                Logs(e);
+            }
+            catch (Sys.FormatException e)
+            {
+                Sys.Console.WriteLine("Enter Valid Numbers Only");
+                Logs(e); 
+            }
+            catch (Sys.OverflowException e)
+            {
+                Sys.Console.WriteLine("The Number is Too Big");
+                Logs(e);
+            }
+            catch (Sys.Exception e)
+            {
+                Sys.Console.WriteLine("Something Went Wrong");
+                Logs(e);
+            }
+        }
+
+        /*static void Main(string [] args)
+        {
+            Division div = new Division();
+            try
+            {
+                div.Excute();
+                div.Display();
+            }
+            catch (Sys.Exception e)
+            {
+                string userMessage = e switch
+                {
+                    Sys.DivideByZeroException => "You Cannot Divide by Zero";
+                    Sys.FormatException => "Enter Valid Numbers Only",
+                    Sys.OverflowException => "The Number is Too Big",
+                    _ => "An unknown error occurred."
+                };
+                Sys.Console.WriteLine(userMessage);
+                Logs(e);
+            }
+        }*/
+
+    }
+}
