4caf5630c19e6e5b3de7473cd4aa213eee2a2c61
Kingsley <acciuskingsley@protonmail.com>
2026-03-21 00:21:06 -0400

    update to Dictionary class

--- Module_7/Collections.cs
+++ Module_7/Collections.cs
@@ -1,8 +1,61 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Collections
 {
+    class DictionaryManager
+    {
+        private Dictionary<string, string> wishlist = new Dictionary<string, string>();
+
+        public void Populate()
+        {
+            wishlist["David"] = "ROG Ally";
+            wishlist["Alica"] = "IPhone 17 Pro Max";
+            wishlist["Claire"] = "Audi R7";
+        }
+
+        public void Display()
+        {
+            if (wishlist.Count == 0)
+            {
+                Console.WriteLine("The Dicttionary is Empty please Pupulate or add new Key Value Pair");
+                return;
+            }
+
+            foreach (KeyValuePair<string, string> wishlistEntry in wishlist)
+            {
+                Console.WriteLine($"Name: {wishlistEntry.Key} | Whish: {wishlistEntry.Value}");
+            }
+        }
+
+        public void RemoveKey(string Key)
+        {
+            if (wishlist.Remove(Key))
+            {
+                Console.WriteLine($"{Key} is Removed");
+            }
+            else
+            {
+                Console.WriteLine($"{Key} is not found");
+            }
+        }
+
+        public void AddNew(string Key, string Value)
+        {
+            if (!wishlist.ContainsKey(Key))
+            {
+                wishlist.Add(Key, Value);
+                Console.WriteLine($"{Key} has been added");
+            }
+            else
+            {
+                Console.WriteLine($"{Key} Already Exists");
+            }
+        }
+    }
+
+
     class Program
     {
         public static void Logs(Sys.Exception e)
