351d454ff0af766a632e513876dde9db189f135c
Kingsley <acciuskingsley@protonmail.com>
2026-03-20 23:08:30 -0400

    Start up

---
+++ Module_7/Collections.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        public static void Logs(Sys.Exception e)
+        {
+                Sys.Console.WriteLine($"Error: {e.Message}");
+        }
+        static void Main(string [] args)
+        {
+            
+        }
+    }
+}
